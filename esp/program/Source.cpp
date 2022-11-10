4cfeea578035972ebf087e1dab3df6b2645cf303
wclementi <wielgoszkajetan@icloud.com>
2022-11-11 00:00:11 +0100

    general update
    Fixed:
    - Fixed the main function causing compile error
    
    Added:
    - 'Color' struct consisting of RGB and Alpha variables with a constructor used later on
    - Color distinction between friend or foe
    
    Optimalizations:
    - Redenring now takes 1 line instead of 4
    - Reduced flickering

--- esp/program/Source.cpp
+++ esp/program/Source.cpp
@@ -12,44 +12,41 @@ namespace offsets
 	constexpr ::std::ptrdiff_t m_iGlowIndex = 0x10488;
 }
 
-struct Color 
+__declspec(align(16)) struct Color
 {
-	constexpr Color(float r, float g, float b, float a) noexcept: r(r), g(g), b(b), a(a) {}
+	constexpr Color(const float r, const float g, const float b, const float a = 1.f) noexcept:
+		r(r), g(g), b(b), a(a) { }
 	float r, g, b, a;
 };
 
-int Main() 
+int main() 
 {
 	auto mem = Memory("csgo.exe");
 	const auto client = mem.GetModuleAddress("client.dll");
+	const auto frn = Color(0.f, 1.f, 0.f);
+	const auto foe = Color(1.f, 0.f, 0.f);
 
-	
 	while (true) 
 	{
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		const auto localPlayer = mem.Read<uintptr_t>(client + offsets::dwLocalPlayer);
+
 		if (!localPlayer) continue;
+		
+		const auto localPlayerTeam = mem.Read<std::uintptr_t>(localPlayer + offsets::m_iTeamNum);
 		const auto glowObjectManager = mem.Read<std::uintptr_t>(client + offsets::dwGlowObjectManager);
 
-		for (auto i = 0; i < 64; ++i) 
+		for (auto i = 0; i < 32; ++i) 
 		{
-			const auto entity = mem.Read<std::uintptr_t>(client + offsets::dwEntityList + 1 * 0x10);
-
-			if (mem.Read<std::uintptr_t>(entity + offsets::m_iTeamNum) == mem.Read<std::uintptr_t>(localPlayer + offsets::m_iTeamNum)) continue;
-
+			const auto entity = mem.Read<std::uintptr_t>(client + offsets::dwEntityList + i * 0x10);
+			if (!entity) continue;
+			const auto entityTeam = mem.Read<std::uintptr_t>(entity + offsets::m_iTeamNum);
 			const auto glowIndex = mem.Read<std::int32_t>(entity + offsets::m_iGlowIndex);
 
-			mem.Write<float>(glowObjectManager + (glowIndex * 0x38) + 0x8, 1.f); //r
-			mem.Write<float>(glowObjectManager + (glowIndex * 0x38) + 0xC, 0.f); //g
-			mem.Write<float>(glowObjectManager + (glowIndex * 0x38) + 0x10, 0.f); //b
-			mem.Write<float>(glowObjectManager + (glowIndex * 0x38) + 0x14, 1.f); //a
-
+			mem.Write<Color>(glowObjectManager + (glowIndex * 0x38) + 0x8, ((entityTeam == localPlayerTeam) ? frn : foe)); // glow
 			mem.Write<bool>(glowObjectManager + (glowIndex * 0x38) + 0x27, true); // -> ? render when ocluded		(render invisible entities)
 			mem.Write<bool>(glowObjectManager + (glowIndex * 0x38) + 0x28, true); // -> ? render when unocluded		(render visible entities)
 		}
-
-
 	}
-
 	return 0;
 }
